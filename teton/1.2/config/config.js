2efc2f8e87582e71999f864a8f65800a350402e2
Samuel <samuel@example.com>
2026-01-30 00:59:51 -0800

    good

---
+++ teton/1.2/config/config.js
@@ -0,0 +1,14 @@
+const LAT = "43.8866";
+const LON = "-111.6777";
+const APIKEY = "1d6da67f766eb0c87c58b8c22f52434a";
+
+const WEATHER_URL_STUB = "./data/weather.json";
+const WEATHER_URL_PROD = `https://api.openweathermap.org/data/2.5/weather?lat=${LAT}&lon=${LON}&appid=${APIKEY}`;
+
+const apiURL = WEATHER_URL_PROD;
+
+const DIRECTORY_DATA_URL_PROD = "<PRODUCTION_URL_HERE>";
+const DIRECTORY_DATA_URL_STUB = "./data/business.json";
+const DIRECTORY_DATA_URL_TEST = "http://127.0.0.1:8000/data/all";
+
+const businessDataUrl = DIRECTORY_DATA_URL_TEST;
